ba0ca44f509665e224a0b7c79f368ca0ac0ae019
Hawas backer <hawas.backer@vonnue.com>
2026-07-08 15:23:13 +0530

    feat: complete week4day1task8 - Create canvas chart from data
    - Drawn a bar chart of 12 month sale data
    - Animated bars growing from zero height
    - Added a toolbar on mouse hover
    - Exported chart as PNG

--- day1/chart.js
+++ day1/chart.js
@@ -40,6 +40,7 @@ function drawBar(ctx, upperLeftX, upperLeftY, width, height, color) {
 let gap = (canvasHeight - padding) / numberOfGridLines;
 let maximumValue = Math.max(...Object.values(data));
 let incrementGrid = Math.ceil(maximumValue / (numberOfGridLines - 1));
+let barHeight = canvasHeight - padding;
 
 function drawGrids() {
     drawLine(ctx, padding, 0, padding, canvasHeight, "black");
@@ -52,21 +53,62 @@ function drawGrids() {
     }
 }
 drawGrids();
+//put 13 just to have a buffer space at the end,increment here means total space taken by a single bar
+let increment = (canvasWidth - padding) / 13;
+let width = increment - 20;
+let obj = {};
+
+function drawGraph(month, percent, gapInBar) {
+    let value = data[month];
+    //this denotes how much pixel a value denotes
+    let ratio = barHeight / numberOfGridLines / incrementGrid;
+    let y = value * ratio;
+    if (percent < 100) {
+        percent++;
+        requestAnimationFrame(function () {
+            drawGraph(month, percent, gapInBar);
+        });
+    }
+    let rectangle = new Path2D();
+    rectangle.rect(gapInBar, barHeight - y, width, y);
+    obj[month] = {
+        rectangle: rectangle,
+        x: gapInBar,
+        y: barHeight - y,
+    };
+
+    let newHeight = (y * percent) / 100;
+    drawBar(ctx, gapInBar, barHeight - newHeight, width, newHeight, "red");
+}
 
 function drawGraphs() {
-    //put 13 just to have a buffer space at the end,increment here means total space taken by a single bar
-    let increment = (canvasWidth - padding) / 13;
-    let width = increment - 20;
     let gapInBar = padding + increment;
     for (const month in data) {
-        let barHeight = canvasHeight - padding;
-        let value = data[month];
-        //this denotes how much pixel a value denotes
-        let ratio = barHeight / numberOfGridLines / incrementGrid;
-        let y = value * ratio;
-        drawBar(ctx, gapInBar, barHeight - y, width, y, "red");
+        let percent = 0;
+        drawGraph(month, percent, gapInBar);
         ctx.fillText(month, gapInBar + 10, canvasHeight - 5);
         gapInBar += increment;
     }
 }
 drawGraphs();
+const toolTip = document.querySelector(".toolTip");
+canvas.addEventListener("mousemove", (event) => {
+    for (const month in obj) {
+        if (
+            ctx.isPointInPath(
+                obj[month].rectangle,
+                event.offsetX,
+                event.offsetY
+            )
+        ) {
+            toolTip.style.top = `${obj[month].y + padding}px`;
+            toolTip.style.left = `${obj[month].x + 5}px`;
+            toolTip.textContent = data[month];
+        }
+    }
+});
+const linkElement = document.querySelector("button");
+linkElement.addEventListener("click", (event) => {
+    let url = canvas.toDataURL("image/png");
+    linkElement.querySelector("a").href = url;
+});
