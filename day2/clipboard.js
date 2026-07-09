40e376fbdf1f2dcdfd94fdb38377ff84d8152031
Hawas backer <hawas.backer@vonnue.com>
2026-07-09 11:07:02 +0530

    feat: added share and fallback for not supported browsers

--- day2/clipboard.js
+++ day2/clipboard.js
@@ -67,11 +67,3 @@ shareButton.addEventListener("click", async (event) => {
         writeToClipboard(url);
     }
 });
-// shareButton.addEventListener("click", async () => {
-//     try {
-//         await navigator.share({ title: "Example Page", url: "" });
-//         console.log("Data was shared successfully");
-//     } catch (err) {
-//         console.error("Share failed:", err.message);
-//     }
-// });
